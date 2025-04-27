0dd8524c3816eda6aea19c2a92d977a483145983
Yousef Ahmed <a.yousef917.ya@gmail.com>
2025-04-28 00:06:11 +0300

    Add project files.

---
+++ ECommerce Project/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce_Project.Controllers
+{
+    [Route("api/[controller]")] //BaseURL +api/ControllerName
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> GetById(int id) //Get =>   BaseURL +api/ControllerName?Id
+        {
+            return new Product { Id = id };
+        }
+        [HttpPut]
+        public ActionResult<Product> Update() //Get =>   BaseURL +api/ControllerName
+        {
+            return new Product { Id = 15 };
+        }
+        [HttpDelete]
+        public ActionResult <Product> Delete(Product product)
+        {
+            return new Product { Id = product.Id,Name=product.Name };
+
+        }
+    }
+
+    public class Product {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ ECommerce Project/Program.cs
@@ -0,0 +1,36 @@
+
+namespace ECommerce_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer(); //Swagger Related
+            builder.Services.AddSwaggerGen(); //Swagger Related
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
